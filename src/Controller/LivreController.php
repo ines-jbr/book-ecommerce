02bd0ee9aed22df4575c33f2254c99bc572ef49a
ines-jbr <inesjabri888@gmail.com>
2026-08-14 17:32:48 +0100

    Ajout formulaire de creation de livre

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Livre;
+use App\Form\LivreType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +20,25 @@ class LivreController extends AbstractController
 
         return $this->render('livre/index.html.twig', [
             'livres' => $livres,
+        ]);
+    }
+    #[Route('/livre/nouveau', name: 'app_livre_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $livre = new Livre();
+        $form = $this->createForm(LivreType::class, $livre);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($livre);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_livre_index');
+        }
+
+        return $this->render('livre/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
---
+++ src/Form/LivreType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Livre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class)
+            ->add('auteur', TextType::class)
+            ->add('description', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('prix', NumberType::class)
+            ->add('stock')
+            ->add('image', TextType::class, [
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
